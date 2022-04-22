3258eff50dcca78f43b408e74a59805ee237fc61
Matthew Duffell-Hoffman <matthew.duffell.h@gmail.com>
2022-04-22 12:34:08 -0400

    Main function used for testing

--- Main.cpp
+++ Main.cpp
@@ -3,63 +3,51 @@
 #include "Plotter.h"
 
 #include "VoltageSource.h"
-#include "Diode.h"
 #include "Resistor.h"
 #include "Capacitor.h"
-
-/*
-
-  Example circuit (rectifier):
-
-     (1)   D1      (2)
-       .--->|---.-------.
-      +|        |       |     +
-  vin (~)    R1 <   C1 ===   vout
-       |        |       |     -
-       '--------+-------'
-               -+- (0)
-                '
-
-  time step = 1e-5 s
-  simulation time = 5 ms
-  
-  vin = 10 * sin(2*pi*1000*t)
-  R1 = 10 Ohm
-  C1 = 1.0 mF
-
-*/
+#include "Converter.h"
 
 int main()
 {
-	const double h = 1e-6;
-	const double tmax = 5e-3;
-	const double Va = 10;
-	const double f = 1000;
-	const double R = 10;
-	const double C = 1e-3;
+	const double h = 0.7e-6;
+	const double tmax = 12e-3;
+	const double Va = 20;
+	const double R = 1e2;
+	const double C = 1e-5;
 
 	Plotter plotter("Project", 1000, 600);
-	plotter.SetLabels("vin (V)", "iR (A)", "vout (V)");
+	plotter.SetLabels("vin (V)", "vout (V)");
 
-	Simulator simulator(2, 0);
+	Simulator simulator(4, 0);
 
-	VoltageSource V1(1, 0, 0, Va, f);
-	Diode D1(1, 2);
-	Resistor R1(2, 0, R);
+	VoltageSource V1(1, 0, Va);
+	Resistor R1(1, 2, R);
 	Capacitor C1(2, 0, C);
+	Resistor R2(3, 4, R);
+	Capacitor C2(3, 4, C);
+	Converter converter(2, 0, 3, 4, 1000, 2e-6, 25.85e-3, 1.5, 1e6, 1e-3, 1);
 
 	simulator.AddDevice(V1);
-	simulator.AddDevice(D1);
 	simulator.AddDevice(R1);
 	simulator.AddDevice(C1);
+	simulator.AddDevice(R2);
+	simulator.AddDevice(C2);
+	simulator.AddDevice(converter);
 
-	simulator.Init(h, tmax);
+	simulator.Init(h, tmax, 100, 1e-6, true);
+
+	converter.SetDuty(0.5);
+	converter.SetMode(-1);
 
 	while (simulator.IsRunning())
 	{
-		plotter.AddRow(simulator.GetTime(), V1.GetVoltage(),
-			R1.GetCurrent(), C1.GetVoltage());
+		plotter.AddRow(simulator.GetTime(), converter.GetInputVoltage(), converter.GetOutputVoltage());
 		
+		if (simulator.GetTime() > tmax / 4 * 3)
+		{
+			converter.SetMode(0);
+		}
+
 		simulator.Step();
 	}
 
